e5e638e02cf50c7ca1c8d66cfd887cb4dd64b484
Hannes Walz <hannes@leuchtetgruen.de>
2018-02-01 15:26:47 +0100

    using styles and adding basic enter code

---
+++ app/src/main/java/de/leuchtetgruen/pluslocation/ui/activities/EnterCodeActivity.kt
@@ -0,0 +1,13 @@
+package de.leuchtetgruen.pluslocation.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import de.leuchtetgruen.pluslocation.R
+
+class EnterCodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_code)
+    }
+}
